f0c795b1ab1d8286526034971142828648212554
sitaramsandeep <52271813+sitaramsandeep@users.noreply.github.com>
2020-03-13 20:50:25 +0530

    Api consuming changes

--- src/components/StarWarList.js
+++ src/components/StarWarList.js
@@ -2,21 +2,6 @@ import React from 'react';
 
 export default class StarWarsList extends React.Component {
 
-  getMoreDetails(homeworld) {
-    fetch(homeworld).then(response => response.json())
-    .then(data => {
-        this.worldDetails = data;
-        this.setState({
-          worldDetails: this.worldDetails
-        })
-        this.componentDidUpdate()
-    })
-  }
-  
-  componentDidUpdate(){
-    this.render()
-  }
-
   render() {
     return (
       <div>
@@ -30,8 +15,8 @@ export default class StarWarsList extends React.Component {
             <span>{this.props.character.gender}</span>
           </li>
         </ul>
-        <button onClick={() => this.getMoreDetails(this.props.character.homeworld)}>Get More Details about <b>{this.props.character.name}</b></button>
-        {/* <div>{!!this.state.worldDetails ? <h1>{this.state.worldDetails.name}</h1>: null}</div> */}
+        <button onClick={() => this.props.getMoreDetails(this.props.character)}>Get More Details about <b>{this.props.character.name}</b></button>
+        { <div>{!!this.props.character.world ? <h1>{this.props.character.world.name}</h1>: null}</div>}
       </div>
     )
   }
